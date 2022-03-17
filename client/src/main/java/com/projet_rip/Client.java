f3af40baee04ac481e9f3d580f6803256b4dd883
subski <seb.beutler@netcourrier.com>
2022-03-17 08:25:12 +0100

    push get/store

--- client/src/main/java/com/projet_rip/Client.java
+++ client/src/main/java/com/projet_rip/Client.java
@@ -8,7 +8,7 @@ import java.util.*;
 import javafx.scene.control.TextArea;
 
 public class Client extends Thread {
-    
+
     final String serverHost = "localhost";
     public Socket socketOfClient = null;
     public PrintStream ps = null;
@@ -21,46 +21,93 @@ public class Client extends Thread {
     private StringBuilder logs = new StringBuilder("");
     private TextArea console_log = null;
 
-    public void run()
-    {
+    public void run() {
         try {
             // Connexion au serveur
             socketOfClient = new Socket(serverHost, 3030);
             ps = new PrintStream(socketOfClient.getOutputStream());
             is = new BufferedReader(new InputStreamReader(socketOfClient.getInputStream()));
-            
+
             String msg_serveur;
+            String[] msg_serveur_split;
             String msg = "";
+            int port;
 
             // Gestion de la réception des paquets
             while (this.continuer && !msg.equals("bye")) {
                 do {
-                msg_serveur = is.readLine();
-                
+                    msg_serveur = is.readLine();
+                    msg_serveur_split = msg_serveur.split(" ");
+
                     switch (msg_serveur.charAt(0)) {
-                        case '0':       
+                        case '0':
                         case '1':
                             System.out.println(msg_serveur);
                             if (msg_serveur.length() >= 2)
                                 logMessage(msg_serveur.substring(2));
-                        break;
+                            break;
                         case '2':
-                        System.err.println(msg_serveur);
-                        if (msg_serveur.length() >= 2)
-                        logMessage(msg_serveur.substring(2));
-                        break;
+                            System.err.println(msg_serveur);
+                            if (msg_serveur.length() >= 2)
+                                logMessage(msg_serveur.substring(2));
+                            break;
                         case '3':
-                        break;
+                            port = Integer.parseInt(msg_serveur_split[1]);
+                            try {
+                                Socket s = new Socket(serverHost, port);
+                                String fichier = msg_serveur_split[2];
+                                FileInputStream inf = new FileInputStream(new File(fichier));
+                                ObjectOutputStream out = new ObjectOutputStream(s.getOutputStream());
+                                byte buf[] = new byte[1024];
+                                int n;
+                                while ((n = inf.read(buf)) != -1) {
+                                    out.write(buf, 0, n);
+                                }
+                                inf.close();
+                                out.close();
+                                s.close();
+                            } catch (IOException e) {
+                                Socket s = new Socket(serverHost, port);
+                                s.close();
+                                System.err.println("Impossible de récuperer le fichier.");
+                            }
+                            break;
                         case '4':
-                        break;
+                            try {
+                                port = Integer.parseInt(msg_serveur_split[1]);
+                                Socket s = new Socket(serverHost, port);
+
+                                String chemin = System.getProperty("user.dir") + "/" + msg_serveur_split[2];
+                                System.out.println(chemin);
+                                File nouveauFichier = new File(chemin);
+
+                                System.out.println("Fichier créé...");
+
+                                FileOutputStream fileOut = new FileOutputStream(nouveauFichier);
+
+                                ObjectInputStream in = new ObjectInputStream(s.getInputStream());
+                                byte buf[] = new byte[1024];
+                                int n;
+                                while ((n = in.read(buf)) != -1) {
+                                    // System.out.println(n);
+                                    fileOut.write(buf, 0, n);
+                                }
+
+                                System.out.println("Fichier copié.");
+                                fileOut.close();
+                                s.close();
+                            } catch (IOException e) {
+                                System.err.println("Impossible de récuperer le fichier.");
+                            }
+                            break;
                         case '5':
                             App.setRoot("primary");
-                        break;
+                            break;
                     }
                 } while (msg_serveur.charAt(0) != '0');
 
                 // Envoie du prochain message
-                msg = msg_client.getMessage();     
+                msg = msg_client.getMessage();
                 logMessage(">> " + msg);
                 ps.println(msg);
             }
@@ -80,7 +127,8 @@ public class Client extends Thread {
         if (console_log != null) {
             try {
                 Thread.sleep(25);
-            } catch (InterruptedException e) {}
+            } catch (InterruptedException e) {
+            }
             this.console_log.setText(logs.toString());
             console_log.appendText("");
         }
@@ -88,15 +136,14 @@ public class Client extends Thread {
 
     // Connexion à l'UI des logs
     public void setupConsoleLog(TextArea textarea) {
-        this.console_log = textarea;        
+        this.console_log = textarea;
         this.console_log.setText(logs.toString());
     }
 
     // Arret serveur
-    public synchronized void stopServeur()
-    {
-        this.continuer = false;        
-        msg_client.setMessage("bye");        
+    public synchronized void stopServeur() {
+        this.continuer = false;
+        msg_client.setMessage("bye");
     }
 
     // -- Commandes --
